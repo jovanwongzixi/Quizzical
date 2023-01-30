ea3cc2e8eacb73991604f2f10f24d2a270f97741
jovanwongzixi <77403495+jovanwongzixi@users.noreply.github.com>
2023-01-30 14:59:05 +0800

    Changed database
    Using RESTful API to get leaderboard data instead of firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import React, {useEffect, useState} from "react";
+import React, {useEffect, useState, useRef} from "react";
 import {initializeApp} from "firebase/app"
 import { getFirestore, setDoc, getDoc, doc } from "firebase/firestore";
 import Homepage from "./Homepage";
 import Quiz from "./Quiz";
 import firebaseConfig from "./firebaseConfig";
+import api from "./api/axiosConfig"
 
-
-const db = getFirestore(initializeApp(firebaseConfig))
+//const db = getFirestore(initializeApp(firebaseConfig))
 //const querySnapshot = await getDoc(doc(db,"leaderboard","jIF4HvapctiqSYb0DJuA"))JSON.parse(process.env.FIREBASE_CONFIG)||
 
 export default function App(){
@@ -18,6 +18,17 @@ export default function App(){
         category: "any",
         difficulty: "any",
     })
+
+    //const revText = useRef()
+    const updateMongoLeaderboard = async(name, score) => {
+        try {
+	        const res = await api.post("/api/leaderboard/add", {name: name, score: score.toString()})
+            console.log("push to leaderboard")
+            console.log(`name: ${name} scoree: ${score}`)
+        } catch (error) {
+            console.log(error)
+        }
+    }
 
     function handleChange(event){
         const {name, value} = event.target
@@ -36,6 +47,7 @@ export default function App(){
         setQuizStarted(prevState => !prevState)
     }
     function updateLeaderboard(data){
+        console.log("Updating leaderboard")
         setLeaderboard(prevLeaderboard => {
             const tempLeaderboard = prevLeaderboard.slice()
             tempLeaderboard.push({
@@ -44,26 +56,44 @@ export default function App(){
             })
             return tempLeaderboard
         })
+        updateMongoLeaderboard(currentPlayer.name, data)
     }
+    
     useEffect(
         () => {
             async function retrieveDocs(){
-                const querySnapshot = await getDoc(doc(db,"leaderboard","jIF4HvapctiqSYb0DJuA"))
-                setLeaderboard(querySnapshot.data()['leaderboard-array'])
+                // const querySnapshot = await getDoc(doc(db,"leaderboard","jIF4HvapctiqSYb0DJuA"))
+                // setLeaderboard(querySnapshot.data()['leaderboard-array'])
+                const res = await api.get("/api/leaderboard/get")
+                setLeaderboard(res.data)
+                console.log(res.data)
             }
             retrieveDocs()
         }, []
     )
-    useEffect(
-        () => {
-            localStorage.setItem("leaderboard", JSON.stringify(leaderboard))
-            if(leaderboard.length > 0) {
-                const docRef = setDoc(doc(db, "leaderboard", "jIF4HvapctiqSYb0DJuA"), {
-                "leaderboard-array": leaderboard
-            })}
-        }
-        , [leaderboard]
-    )
+    // useEffect(
+    //     () => {
+    //         async function addLeaderboard(){
+    //             console.log("updating leaderboard")
+    //             try {
+    //                 if(currentPlayer.name) {
+    //                             //     const docRef = setDoc(doc(db, "leaderboard", "jIF4HvapctiqSYb0DJuA"), {
+    //                             //     "leaderboard-array": leaderboard
+                                    
+    //                             // })
+    //                     console.log(currentPlayer.name)
+    //                     const res = await api.post("/api/leaderboard/add", {name:currentPlayer, score: leaderboard[currentPlayer]})
+    //                     console.log(res)
+    //                 }
+    //             } catch (error) {
+    //                 console.log(error)
+    //             }
+    //         }
+    //         //localStorage.setItem("leaderboard", JSON.stringify(leaderboard))
+    //         addLeaderboard()
+    //     }
+    //     , [leaderboard]
+    // )
     return(
         <>
             {
